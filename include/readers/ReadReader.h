82c732d252c768eb764b3c237e50492959dc2347
Ilias Tsatiris <eliastsatiris@gmail.com>
2019-03-24 23:04:27 +0200

    Move ReadReader constructor impl to .cpp file

--- include/readers/ReadReader.h
+++ include/readers/ReadReader.h
@@ -6,11 +6,7 @@
 
 class ReadReader : public FileReader {
 public:
-    ReadReader(int fd, uint64_t from, uint64_t to, uint64_t chunk_size) :
-            FileReader(fd, from, to, chunk_size)
-    {
-        buffer = static_cast<unsigned char*>(malloc(chunk_size * sizeof(unsigned char)));
-    }
+    ReadReader(int fd, uint64_t from, uint64_t to, uint64_t chunk_size);
 
     virtual unsigned char* next(uint64_t *sz);
 
--- src/readers/ReadReader.cpp
+++ src/readers/ReadReader.cpp
@@ -1,7 +1,14 @@
 #include <unistd.h>
 #include <cstdlib>
+#include <fcntl.h>
 
 #include "ReadReader.h"
+
+ReadReader::ReadReader(int fd, uint64_t from, uint64_t to, uint64_t chunk_size) :
+    FileReader(fd, from, to, chunk_size)
+{
+    buffer = static_cast<unsigned char*>(malloc(chunk_size * sizeof(unsigned char)));
+}
 
 ReadReader::~ReadReader() {
     free(buffer);
