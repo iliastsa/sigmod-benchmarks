bc1eca99ea8b510153134c71d8ebf53395871761
Karystinos Nikolaos <nikoskar31@gmail.com>
2019-03-25 07:32:11 +0200

    add templates in HistogramTask to support dynamic reader choice

--- include/parallel/HistogramTask.h
+++ include/parallel/HistogramTask.h
@@ -1,20 +1,36 @@
 #ifndef SIGMODBENCHMARKS_HISTOGRAMTASK_H
 #define SIGMODBENCHMARKS_HISTOGRAMTASK_H
 
+#include <Partition.h>
 #include "ThreadPool.h"
 #include "FileReader.h"
 
-
+template <class T>
 class HistogramTask : public ThreadPool::task {
 private:
     FileReader* reader;
     uint64_t *histogram;
 
 public:
-    HistogramTask(uint64_t* histogram, int fd, uint64_t from, uint64_t to, uint64_t chunk_size);
-    ~HistogramTask() final;
+    HistogramTask(uint64_t* histogram, int fd, uint64_t from, uint64_t to, uint64_t chunk_size) :
+            histogram(histogram) {
+        reader = new T(fd, from, to, chunk_size);
+    }
 
-    void run() final;
+    ~HistogramTask() final {
+        delete reader;
+    }
+
+    void run() final {
+        Partition partition(histogram);
+
+        uint64_t sz;
+        unsigned char* mem;
+
+        while ((mem = reader->next(&sz)) != nullptr)
+            partition.update_histogram(mem, sz);
+
+    }
 };
 
 #endif
