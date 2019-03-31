83e8f6b71341d28171fc6190bf7cedf3094419c9
Ilias Tsatiris <eliastsatiris@gmail.com>
2019-03-31 05:26:54 +0300

    Change some types to uint64_t, restore borrowedsort.

--- include/structures/Tuple.h
+++ include/structures/Tuple.h
@@ -7,7 +7,7 @@
 
 struct Tuple {
     unsigned char key[10];
-    int32_t rowID;
+    uint32_t rowID;
 
     bool operator<(const Tuple &rhs) const {
         return memcmp(key,rhs.key,Constants::KEY_SIZE) < 0;
--- include/utilities/Constants.h
+++ include/utilities/Constants.h
@@ -10,8 +10,8 @@ namespace Constants {
 
     const uint16_t N_PARTITIONS = 1024;
 
-    const uint16_t KEY_SIZE   = 10;
-    const uint16_t TUPLE_SIZE = 100;
+    const uint64_t KEY_SIZE   = 10;
+    const uint64_t TUPLE_SIZE = 100;
     const uint16_t CHUNK_SIZE = 4000; // If CHUNK_SIZE is 0, the whole thread segment will be read
 }
 
